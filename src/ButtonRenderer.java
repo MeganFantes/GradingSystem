61cf5399881e92c1013ff229af19e9768b135d8d
yueh ying lee <yylee@media.ee.ntu.edu.tw>
2019-04-19 02:02:31 -0400

    missing keyword public if not only default package

--- src/ButtonRenderer.java
+++ src/ButtonRenderer.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 
-class ButtonRenderer extends JButton implements TableCellRenderer {
+public class ButtonRenderer extends JButton implements TableCellRenderer {
 
 	public ButtonRenderer() {
 		setOpaque(true);
